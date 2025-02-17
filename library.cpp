b5790d88625f0950488af14074acae0c9904c9a6
Michael Brunner <brunner@cadwork.swiss>
2025-02-17 16:41:32 +0100

    run server in event loop

--- library.cpp
+++ library.cpp
@@ -5,81 +5,76 @@
 #include <QLabel>
 #include <QFile>
 #include <QStandardPaths>
+#include <QEventLoop>
 
 #include <cwapi3d/CwAPI3D.h>
 
-class ServerDialog final: public QDialog
+class ServerHandler final: public QObject
 {
     Q_OBJECT
 
 public:
-    explicit ServerDialog(CwAPI3D::UtilityController *utilityController, QWidget *parent = nullptr)
-        : QDialog(parent),
+    explicit ServerHandler(CwAPI3D::UtilityController *utilityController, QObject *parent = nullptr)
+        : QObject(parent),
           server(new QTcpServer(this)),
           utilityController(utilityController)
     {
-        setWindowTitle("TCP Server");
-        resize(300, 150);
+        connect(server, &QTcpServer::newConnection, this, &ServerHandler::handleNewConnection);
 
-        const auto layout = new QVBoxLayout(this);
-        statusLabel = new QLabel("Waiting for connection...", this);
-        layout->addWidget(statusLabel);
-
-        connect(server, &QTcpServer::newConnection, this, &ServerDialog::handleNewConnection);
         if (!server->listen(QHostAddress::Any, 9999)) {
             qDebug() << "Server failed to start: " << server->errorString();
+        } else {
+            qDebug() << "Server listening on port 9999...";
         }
-
-        setAttribute(Qt::WA_DeleteOnClose);
     }
+
+    void runEventLoop() const
+    {
+        QEventLoop loop;
+        connect(this, &ServerHandler::serverStopped, &loop, &QEventLoop::quit);
+        loop.exec();
+    }
+
+signals:
+    void serverStopped();
 
 private slots:
     void handleNewConnection()
     {
         QTcpSocket *clientSocket = server->nextPendingConnection();
-        statusLabel->setText("Client Connected!");
+        qDebug() << "Client Connected!";
 
-        connect(clientSocket,
-                &QTcpSocket::disconnected,
-                this,
-                [=]()
-                {
-                    statusLabel->setText("Client Disconnected");
-                    clientSocket->deleteLater();
-                });
+        connect(clientSocket, &QTcpSocket::disconnected, this, [=]() {
+            qDebug() << "Client Disconnected";
+            clientSocket->deleteLater();
+        });
 
-        connect(clientSocket,
-                &QTcpSocket::readyRead,
-                this,
-                [=]()
-                {
-                    const QByteArray data = clientSocket->readAll();
-                    statusLabel->setText("Data Received");
+        connect(clientSocket, &QTcpSocket::readyRead, this, [=]() {
+            const QByteArray data = clientSocket->readAll();
+            qDebug() << "Data Received: " << data;
 
-                    const QString tempPath = QStandardPaths::writableLocation(QStandardPaths::TempLocation) +
-                        "/cw_script.py";
-                    QFile file(tempPath);
-                    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-                        file.write(data);
-                        file.close();
-                    }
-                    if (file.exists()) {
-                        utilityController->runExternalProgramFromCustomDirectory(tempPath.toStdWString().c_str());
-                    }
-                    QFile::remove(tempPath);
-                });
+            const QString tempPath = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "/cw_script.py";
+            QFile file(tempPath);
+            if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+                file.write(data);
+                file.close();
+            }
+            if (file.exists()) {
+                utilityController->runExternalProgramFromCustomDirectory(tempPath.toStdWString().c_str());
+            }
+            QFile::remove(tempPath);
+        });
     }
 
 private:
     QTcpServer *server{nullptr};
-    QLabel *statusLabel{nullptr};
     CwAPI3D::UtilityController *utilityController{nullptr};
 };
 
 CWAPI3D_PLUGIN bool plugin_x64_init(CwAPI3D::ControllerFactory *aFactory)
 {
-    const auto serverDialog = new ServerDialog(aFactory->getUtilityController());
-    serverDialog->show();
+    const auto serverHandler = new ServerHandler(aFactory->getUtilityController());
+    serverHandler->runEventLoop();
 
     return false;
 }
